0586c206fa727c1c32e383ceda8eff6e2d3c60b9
RudDavid <satorugodze17@gmail.com>
2026-06-28 22:22:15 +0700

    ........

---
+++ Strategy/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+interface IPayment {
+  void Pay();
+}
+
+class CardPayment : IPayment {
+  public void Pay() {
+    Console.WriteLine("Оплата картой");
+  }
+}
+
+class CashPayment : IPayment {
+  public void Pay() {
+    Console.WriteLine("Оплата наличными");
+  }
+}
+
+class Shop {
+  private IPayment payment;
+
+  // Конструктор получает способ оплаты
+  public Shop(IPayment _payment) {
+    payment = _payment;
+  }
+
+  // Покупка
+  public void Buy() {
+    payment.Pay();
+  }
+}
+
+class Program {
+  static void Main(string[] args) {
+    Shop shop1 = new Shop(new CardPayment());
+    shop1.Buy();
+
+    Shop shop2 = new Shop(new CashPayment());
+    shop2.Buy();
+  }
+}
