d426a9b19a3e7e86d5362786f1c21bccc28a253f
RudDavid <satorugodze17@gmail.com>
2026-06-28 21:50:31 +0700

    ..

---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Person {
+  public string Name;
+
+  // Метод Clone создает копию текущего объекта
+  public Person Clone() {
+    return (Person)this.MemberwiseClone();
+  }
+}
+
+class Program {
+  static void Main(string[] args) {
+    // Создаем первый объект
+    Person p1 = new Person();
+    p1.Name = "Ivan";
+
+    // Создаем копию объекта
+    Person p2 = p1.Clone();
+
+    p2.Name = "Alex";
+
+    Console.WriteLine("Первый объект: " + p1.Name);
+    Console.WriteLine("Второй объект: " + p2.Name);
+  }
+}
