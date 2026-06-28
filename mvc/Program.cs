e4a2c5c0d6f81b503e40a17e5b9cbc36d69962be
RudDavid <satorugodze17@gmail.com>
2026-06-28 22:35:10 +0700

    12

---
+++ mvc/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace mvc {
+  class Model {
+    public string Text = "Hello";
+  }
+
+  class View {
+    public void Show(string text) {
+      Console.WriteLine(text);
+    }
+  }
+
+  class Controller {
+    private Model model = new Model();
+    private View view = new View();
+
+    public void Display() {
+      view.Show(model.Text);
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      Controller controller = new Controller();
+
+      controller.Display();
+    }
+  }
+}
