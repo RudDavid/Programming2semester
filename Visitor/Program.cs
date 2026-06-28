d9eec3005733cbdc690a27e9ade755f8606ec8b0
RudDavid <satorugodze17@gmail.com>
2026-06-28 22:25:31 +0700

    .........

---
+++ Visitor/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Visitor {
+
+  // Интерфейс посетителя
+  interface IVisitor {
+    void Visit(Book book);
+  }
+
+  class Book {
+    public string Title = "C#";
+
+    // Передаем книгу посетителю
+    public void Accept(IVisitor visitor) {
+      visitor.Visit(this);
+    }
+  }
+
+  // Посетитель
+  class PrintVisitor : IVisitor {
+    public void Visit(Book book) {
+      Console.WriteLine(book.Title);
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      Book book = new Book();
+
+      PrintVisitor visitor = new PrintVisitor();
+
+      book.Accept(visitor);
+    }
+  }
+}
