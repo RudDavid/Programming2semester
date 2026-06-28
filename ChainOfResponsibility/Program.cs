11d3dda6141c9315ab40be64a8427a2ee430b9e9
RudDavid <satorugodze17@gmail.com>
2026-06-28 22:18:50 +0700

    .......

---
+++ ChainOfResponsibility/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+
+// Базовый обработчик
+abstract class Handler {
+  protected Handler next;
+
+  // Устанавливаем следующего обработчика
+  public void SetNext(Handler handler) {
+    next = handler;
+  }
+
+  // Метод обработки запроса
+  public abstract void Handle(string request);
+}
+
+class Manager : Handler {
+  public override void Handle(string request) {
+    if (request == "Отпуск") {
+      Console.WriteLine("Менеджер обработал заявку");
+    } else if (next != null) {
+      next.Handle(request);
+    }
+  }
+}
+
+class Director : Handler {
+  public override void Handle(string request) {
+    Console.WriteLine("Директор обработал заявку");
+  }
+}
+
+class Program {
+  static void Main(string[] args) {
+    Manager manager = new Manager();
+    Director director = new Director();
+
+    // Соединяем их в цепочку
+    manager.SetNext(director);
+
+    manager.Handle("Отпуск");
+  }
+}
