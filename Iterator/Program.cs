e11fc312295a9850968b5f4d798790c0000180cd
RudDavid <satorugodze17@gmail.com>
2026-06-28 22:00:22 +0700

    ....

---
+++ Iterator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterator {
+  class Program {
+    static void Main(string[] args) {
+      // Создаем коллекцию студентов
+      List<string> students = new List<string>();
+
+      // Добавляем элементы в коллекцию
+      students.Add("Ivan");
+      students.Add("Alex");
+      students.Add("Maria");
+
+      // Последовательно перебираем элементы коллекции
+      foreach (string student in students) {
+        Console.WriteLine(student);
+      }
+    }
+  }
+}
