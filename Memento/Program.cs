1266e74bbc6663e0ffad794e2e2d1b39da172887
RudDavid <satorugodze17@gmail.com>
2026-06-28 21:56:16 +0700

    ...

---
+++ Memento/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Memento {
+  class Memento {
+    // Поле хранит сохраненное состояние
+    public string State;
+
+    // Конструктор сохраняет состояние
+    public Memento(string state) {
+      State = state;
+    }
+  }
+
+  class Editor {
+    // Текущее состояние редактора
+    public string Text;
+
+    // Метод сохраняет состояние
+    public Memento Save() {
+      return new Memento(Text);
+    }
+
+    // Метод восстанавливает состояние
+    public void Restore(Memento memento) {
+      Text = memento.State;
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      Editor editor = new Editor();
+
+      // Первая версия текста
+      editor.Text = "Первая версия";
+
+      // Сохраняем состояние
+      Memento save = editor.Save();
+
+      // Изменяем текст
+      editor.Text = "Вторая версия";
+
+      Console.WriteLine(editor.Text);
+
+      // Восстанавливаем сохранение
+      editor.Restore(save);
+
+      Console.WriteLine(editor.Text);
+    }
+  }
+}
