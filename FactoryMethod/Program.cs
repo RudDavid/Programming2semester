ae96fd53cc2d11e862072e467a2024b3c1bcc43f
RudDavid <satorugodze17@gmail.com>
2026-06-28 22:28:36 +0700

    ..........

---
+++ FactoryMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FactoryMethod {
+  interface ITransport {
+    void Drive();
+  }
+
+  class Car : ITransport {
+    public void Drive() {
+      Console.WriteLine("Едет автомобиль");
+    }
+  }
+
+  class Bike : ITransport {
+    public void Drive() {
+      Console.WriteLine("Едет велосипед");
+    }
+  }
+
+  // Фабрика
+  class TransportFactory {
+    public ITransport Create(string type) {
+      if (type == "Car")
+        return new Car();
+
+      return new Bike();
+    }
+  }
+
+  class Program {
+    static void Main(string[] args) {
+      TransportFactory factory = new TransportFactory();
+
+      ITransport transport = factory.Create("Car");
+
+      transport.Drive();
+    }
+  }
+}
