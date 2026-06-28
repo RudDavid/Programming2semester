5df8e297d661704d86b26fe950950cfe3952a668
RudDavid <satorugodze17@gmail.com>
2026-06-28 21:42:57 +0700

    .

---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace SingletonPattern {
+  public class Singleton {
+    public static Singleton s_Instance {
+      get {
+        if (instance == null) instance = new Singleton();
+        return instance;
+      }
+    }
+    public void Method1() { 
+      Console.WriteLine("Singleton.Method1"); 
+    }
+    public void Method2() { 
+      Console.WriteLine("Singleton.Method2"); 
+    }
+    private Singleton() { 
+    }
+    private static Singleton instance;
+  }
+  class Program {
+    static void Main(string[] args) {
+      Singleton.s_Instance.Method1();
+      Singleton.s_Instance.Method2();
+    }
+  }
+}
